d19f0348dc9702ca351323e6c7af665eec87c705
Rus <mamail>
2021-03-30 12:48:51 +0300

    New project 5

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slide/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slide/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
